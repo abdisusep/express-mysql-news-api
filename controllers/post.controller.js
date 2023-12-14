37e352a25217712705b73c0305ef078ead121a6b
Susep Dev <susep.dev@gmail.com>
2023-12-14 21:08:54 +0700

    Update controller

--- controllers/post.controller.js
+++ controllers/post.controller.js
@@ -1,21 +1,53 @@
+const service = require('../services/post.service');
+
 const getPosts = async (req, res) => {
-    res.send('Get');
+    try {
+        const posts = await service.getPosts();
+        res.status(200).json(posts);
+    } catch (e) {
+        res.status(400).json({ error: e.message });
+    }
 }
 
 const getPost = async (req, res) => {
-    res.send('Post');
+    try {
+        const id   = req.params.id;
+        const post = await service.getPost(id);
+        res.status(200).json(post);
+    } catch (e) {
+        res.status(500).json({ error: e.message });
+    }
 }
 
 const createPost = async (req, res) => {
-    res.send('Post');
+    try {
+        const data = req.body;
+        const post = await service.createPost(data);
+        res.status(201).json(post);
+    } catch (e) {
+        res.status(400).json({ error: e.message });
+    }
 }
 
 const updatePost = async (req, res) => {
-    res.send('Post');
+    try {
+        const data = req.body;
+        const id   = req.params.id;
+        const post = await service.updatePost(data, id);
+        res.status(200).json(post);
+    } catch (e) {
+        res.status(400).json({ error: e.message });
+    }
 }
 
 const deletePost = async (req, res) => {
-    res.send('Post');
+    try {
+        const id   = req.params.id;
+        const post = await service.deletePost(id);
+        res.status(200).json(post);
+    } catch (e) {
+        res.status(400).json({ error: e.message });
+    }
 }
 
 module.exports = {
