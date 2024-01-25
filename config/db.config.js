9fbeac037450892252aba1b16a66572dcd35655f
Susep Sp <susep.dev@gmail.com>
2024-01-25 17:06:33 +0700

    Update db.config.js

--- config/db.config.js
+++ config/db.config.js
@@ -6,7 +6,7 @@ const sequelize = new Sequelize(
     process.env.DB_USER, 
     process.env.DB_PASS, {
         host: process.env.DB_HOST,
-        dialect: require('pg')
+        dialect: 'postgres'
     }
 );
 
