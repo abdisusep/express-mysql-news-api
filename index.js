4ea167687000ce5c29ee04996faf558fbfe6b94f
Susep Dev <susep.dev@gmail.com>
2024-01-25 11:53:57 +0700

    Update

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const helmet = require('helmet');
 const mainRoutes = require('./routes'); 
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
